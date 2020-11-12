c6b58df5b2d7df4109d1deac89f7b30adcbf978d
pkulcsarsz <44004765+pkulcsarsz@users.noreply.github.com>
2020-11-12 17:23:04 +0100

    Rework main file

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import data
 import models
-# from sklearn.model_selection import train_test_split
+import modelsWithFeatures
+import sys, getopt
 
 # Define basic variables
 num_classes = 36
 inputShape = (32, 32, 3)
+dataset = 'dataset2'
+
+selectedModel = ""
+
+opts, args = getopt.getopt(sys.argv[1:], 'md:')
+for opt, arg in opts:
+    if opt == '-m':
+        selectedModel = arg
+    if opt == '-d':
+        dataset = arg
+
+if(dataset != 'dataset1' and dataset != 'dataset2'):
+        print("Dataset is not recognized")
+        raise
+
+if selectedModel == 'VGG':
+        model = modelsWithFeatures.VGG(inputShape, num_classes, steps_per_epoch = 30, epochs = 40, use_cache=True, dataset=dataset)
+
+if selectedModel == 'customVGG':
+        model = modelsWithFeatures.customVGG(inputShape, num_classes, steps_per_epoch = 30, epochs = 40, use_cache=True, dataset=dataset)
+
+
 
 # [X, Y] = data.load_images('dataset1/train', 0, 5, num_classes, False, True)
 
@@ -55,6 +78,4 @@ inputShape = (32, 32, 3)
 #         steps_per_epoch = 30, epochs = 40, use_cache=True, dataset=dataset)
 
 
-dataset = 'dataset2'
-model = models.customVGG(inputShape, num_classes,
-        steps_per_epoch = 30, epochs = 40, use_cache=True, dataset=dataset)
+
