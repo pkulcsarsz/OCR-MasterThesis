057769b5b493ec398895ba5bb85f1bbf75ee349d
pkulcsarsz <44004765+pkulcsarsz@users.noreply.github.com>
2020-11-27 15:35:16 +0100

    Fix

--- modelsWithFeatures.py
+++ modelsWithFeatures.py
@@ -213,7 +213,7 @@ def fitModel(model, dataset, input_shape, batch_size, epochs, addCharacteristics
     my_validation_batch_generator = CustomGenerator(validation_filenames, GT_validation, batch_size)
 
     start = time.time()
-    history = model.fit(generator=my_training_batch_generator,
+    history = model.fit(my_training_batch_generator,
                                             steps_per_epoch=len(training_filenames)/batch_size,
                                             epochs=epochs,
                                             verbose=1,
