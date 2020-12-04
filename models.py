aa9d4481f44ea3808792406f9e8c3b520e105104
Peter Kulcsar Szabo <kulcsar@profit365.eu>
2020-12-04 10:28:18 +0100

    Working on code models

--- models.py
+++ models.py
@@ -157,7 +157,7 @@ def customLeNet2(input_shape, num_classes, steps_per_epoch, epochs, use_cache=Fa
     return model
 
 
-def mLeNet2(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
+def mLeNetEnhanced(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
     model_name = 'LeNet'
     helpers.createFoldersForModel(model_name, dataset)
     print("===================== " + model_name + " model ====================")
@@ -235,53 +235,68 @@ def mLeNetDefault_Features(input_shape, num_classes, steps_per_epoch, epochs, us
     model_name = 'LeNetDefault_Features'
     helpers.createFoldersForModel(model_name, dataset)
     print("===================== " + model_name + " model ====================")
+    
+    losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
+    metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
+    weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
+
     if existsModelCache(model_name, dataset) and use_cache:
         model = loadModel(model_name, dataset)
-        model.compile(loss='categorical_crossentropy',
-                      optimizer='rmsprop', metrics=['accuracy'])
-        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs)
+        model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
+        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs, True)
         return model
 
     model = getDefaultModel(input_shape)
     # flat1 = Flatten()(model.layers[-1].output)
-
     # dense1 = Dense(units=256, activation="relu")(flat1)
     classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(model.layers[-1].output)
-
     featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
-
     # define new model
     model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
     # summarize
     model.summary()
-
-    n = 13 # number of features
-    k = num_classes #number of classes
-    cce = tf.keras.losses.CategoricalCrossentropy()
-    def featuresLossFunction(y_true, y_pred):
-        return K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:])
-
-    def classifierLossFunction(y_true, y_pred):
-        return cce(y_true[:,0:k], y_pred)
-
-    def classifierAccuracy(y_true,y_pred):
-        return tf.keras.metrics.sparse_categorical_accuracy(tf.math.argmax(y_true[:,0:k],1), y_pred)
-
-    def featuresAccuracy(y_true, y_pred):
-        
-        return K.sum(K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:]))
-
-    tf.executing_eagerly()
-
-    losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
-    metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
-    weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
 
     model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
     model.run_eagerly = True
     [history, time_taken] = fitModel(
         model, dataset, input_shape, steps_per_epoch, epochs, True)
 
+    if use_cache:
+        saveModel(model, model_name, dataset)
+
+    createAndSaveCurvesFeatures(history, model_name, dataset)
+
+    return model
+    
+def mLeNetDefault_Features_Dense(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
+    model_name = 'LeNetDefault_Features'
+    helpers.createFoldersForModel(model_name, dataset)
+    print("===================== " + model_name + " model ====================")
+    
+    losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
+    metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
+    weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
+
+    if existsModelCache(model_name, dataset) and use_cache:
+        model = loadModel(model_name, dataset)
+        model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
+        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs, True)
+        return model
+
+    model = getDefaultModel(input_shape)
+    # flat1 = Flatten()(model.layers[-1].output)
+    dense1 = Dense(units=64, activation="relu")(model.layers[-1].output)
+    classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(dense1)
+    featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
+    # define new model
+    model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
+    # summarize
+    model.summary()
+
+    model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
+    model.run_eagerly = True
+    [history, time_taken] = fitModel(
+        model, dataset, input_shape, steps_per_epoch, epochs, True)
 
     if use_cache:
         saveModel(model, model_name, dataset)
@@ -321,16 +336,37 @@ def fitModel(model, dataset, input_shape, batch_size, epochs, addCharacteristics
 
 
 
-def evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs):
-    test_datagen = ImageDataGenerator(
-        featurewise_center=True, featurewise_std_normalization=True, rescale=1. / 255)
-    validation_generator = test_datagen.flow_from_directory(
-        getValidationDatasetPath(dataset),
-        target_size=(input_shape[0], input_shape[1]),
-        batch_size=steps_per_epoch,
-        class_mode='categorical')
+def evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs, addCharacteristics = False):
+    # test_datagen = ImageDataGenerator(
+    #     featurewise_center=True, featurewise_std_normalization=True, rescale=1. / 255)
+    # validation_generator = test_datagen.flow_from_directory(
+    #     getValidationDatasetPath(dataset),
+    #     target_size=(input_shape[0], input_shape[1]),
+    #     batch_size=steps_per_epoch,
+    #     class_mode='categorical')
+    [validation_filenames, GT_validation] = load_image_names_and_labels(dataset, 'validation', addCharacteristics)
+    my_validation_batch_generator = CustomGenerator(validation_filenames, GT_validation, batch_size)
 
     print("================= Evaluating the model =================")
-    scores = model.evaluate_generator(validation_generator)
+    scores = model.evaluate(my_validation_batch_generator)
     print("%s: %.2f" % (model.metrics_names[0], scores[0]))
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    
+def featuresLossFunction(y_true, y_pred):
+    k = 36 # number of classes
+    n = 13 # number of features
+    return K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:])
+
+def classifierLossFunction(y_true, y_pred):
+    k = 36 # number of classes
+    cce = tf.keras.losses.CategoricalCrossentropy()
+    return cce(y_true[:,0:k], y_pred)
+
+def classifierAccuracy(y_true,y_pred):
+    k = 36 # number of classes
+    return tf.keras.metrics.sparse_categorical_accuracy(tf.math.argmax(y_true[:,0:k],1), y_pred)
+
+def featuresAccuracy(y_true, y_pred):
+    k = 36 # number of classes
+    n = 13 # number of features
+    return K.sum(K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:]))
