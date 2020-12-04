d6fb027f8e2eef4a88cf83d23c63a0b4fd519f04
Peter Kulcsar Szabo <kulcsar@profit365.eu>
2020-12-04 09:15:24 +0100

    defautl lenet with features

--- modelResults.py
+++ modelResults.py
@@ -18,66 +18,14 @@ def loadImageFromPath(path):
 
   return image
 
-def createResultsForModels2(modelsArrays, modelsAreFeatures = [], dataset = 'dataset3'):
-    total = 0
-    amountOfModels = len(modelsArrays)
-    cols = 1 + amountOfModels + amountOfModels^2
-    results = np.zeros((37, cols))
-    imagesToTest = [None]*(len(modelsArrays) + 1)
-
-    for letter in letters:
-        label_index = get_label_index(letters, letter)
-        for filename in os.listdir(dataset + '/validation/' + letter):
-            imagesToTest[0] = loadImageFromPath(dataset + '/validation/' + letter + '/' + filename)
-            for i in range(len(modelsArrays)):
-                imagesToTest[i + 1] = generate_adversarial(modelsArrays[i], imagesToTest[0], tf.one_hot(label_index, 36), 0.2, True)
-            for i in range(len(modelsArrays)):
-                #the basic image on each model
-                if modelsAreFeatures[i] == True:
-                    if get_label(letters, modelsArrays[i].predict(imagesToTest[0])[0])[0] == letter:
-                        results[label_index, i*len(modelsArrays)] += 1
-                        results[-1, i*len(modelsArrays)] += 1
-                else:
-                    if get_label(letters, modelsArrays[i].predict(imagesToTest[0]))[0] == letter:
-                        results[label_index,i*len(modelsArrays)] += 1
-                        results[-1, i*len(modelsArrays)] += 1
-                #iterate trough adversarial samples for each model
-                for j in range(len(modelsArrays)):
-                    if modelsAreFeatures[i] == True:
-                        if get_label(letters, modelsArrays[i].predict(imagesToTest[j + 1])[0])[0] == letter:
-                            results[label_index, i*len(modelsArrays) + 1 + j] += 1
-                            results[-1, i*len(modelsArrays) + 1 + j] += 1
-                    else:
-                        if get_label(letters, modelsArrays[i].predict(imagesToTest[j + 1]))[0] == letter:
-                            results[label_index,i*len(modelsArrays) + 1 + j] += 1
-                            results[-1, i*len(modelsArrays) + 1 + j] += 1
-
-            #Increment the counter for each letter
-            results[label_index,0] += 1
-            total += 1
-            if total % 250 == 0 :
-                print("Current ===== ", total)
-
-        #divide succes counts with total count of letter
-        results[label_index, 1:] = results[label_index, 1:] / results[label_index, 0]
-
-    results[-1, 1:] = results[-1, 1:] / results[-1, 0]
-    print("Done")
-
 
 def createResultsForModels(trained_model, trained_model_features, trained_model_features2):
     succ = 0
     succ_adv = 0
     succ_adv_f = 0
-    succ_adv_f2 = 0
     succ_f = 0
     succ_f_adv = 0
     succ_f_adv_f = 0
-    succ_f_adv_f2 = 0
-    succ_f2 = 0
-    succ_f2_adv = 0
-    succ_f2_adv_f = 0
-    succ_f2_adv_f2 = 0
     total = 0
 
     results = np.zeros((37,13))
@@ -90,31 +38,18 @@ def createResultsForModels(trained_model, trained_model_features, trained_model_
             # Generate adversarial sample 
             imToTest_adv = generate_adversarial(trained_model, imToTest, tf.one_hot(label_index, 36), 0.2, False)        
             imToTest_adv_features = generate_adversarial(trained_model_features, imToTest, tf.one_hot(label_index, 36), 0.2, True)
-            imToTest_adv_features2 = generate_adversarial(trained_model_features2, imToTest, tf.one_hot(label_index, 36), 0.2, True)
             # Get perdictions of the original and adversarial sample for basic model
             label = trained_model.predict(imToTest)
             label_adv = trained_model.predict(imToTest_adv)
             label_adv_f = trained_model.predict(imToTest_adv_features)
-            label_adv_f2 = trained_model.predict(imToTest_adv_features2)
             # Get perdictions of the original and adversarial sample for features model
             label_f = trained_model_features.predict(imToTest)
             label_f_adv = trained_model_features.predict(imToTest_adv)
             label_f_adv_f = trained_model_features.predict(imToTest_adv_features)
-            label_f_adv_f2 = trained_model_features.predict(imToTest_adv_features2)
-            # Get perdictions of the original and adversarial sample for features model2
-            label_f2 = trained_model_features2.predict(imToTest)
-            label_f2_adv = trained_model_features2.predict(imToTest_adv)
-            label_f2_adv_f = trained_model_features2.predict(imToTest_adv_features)
-            label_f2_adv_f2 = trained_model_features2.predict(imToTest_adv_features2)
             # Get label for classification from features labels
             label_f = label_f[0]
             label_f_adv = label_f_adv[0]
             label_f_adv_f = label_f_adv_f[0]
-            label_f_adv_f2 = label_f_adv_f2[0]
-            label_f2 = label_f2[0]
-            label_f2_adv = label_f2_adv[0]
-            label_f2_adv_f = label_f2_adv_f[0]
-            label_f2_adv_f2 = label_f2_adv_f2[0]
             #Check if the predictions were correct
             if (get_label(letters, label)[0] == letter):
                 succ = succ + 1
@@ -125,36 +60,17 @@ def createResultsForModels(trained_model, trained_model_features, trained_model_
             if (get_label(letters, label_adv_f)[0] == letter):
                 succ_adv_f = succ_adv_f + 1
                 results[label_index,3] += 1
-            if (get_label(letters, label_adv_f2)[0] == letter):
-                succ_adv_f2 = succ_adv_f2 + 1
-                results[label_index,4] += 1
             
             if (get_label(letters, label_f)[0] == letter):
                 succ_f = succ_f + 1
-                results[label_index,5] += 1
+                results[label_index,4] += 1
             if (get_label(letters, label_f_adv)[0] == letter):
                 succ_f_adv = succ_f_adv + 1
-                results[label_index,6] += 1
+                results[label_index,5] += 1
             if (get_label(letters, label_f_adv_f)[0] == letter):
                 succ_f_adv_f = succ_f_adv_f + 1
-                results[label_index,7] += 1
-            if (get_label(letters, label_f_adv_f2)[0] == letter):
-                succ_f_adv_f2 = succ_f_adv_f2 + 1
-                results[label_index,8] += 1
+                results[label_index,6] += 1
             
-            if (get_label(letters, label_f2)[0] == letter):
-                succ_f2 = succ_f2 + 1
-                results[label_index,9] += 1
-            if (get_label(letters, label_f2_adv)[0] == letter):
-                succ_f2_adv = succ_f2_adv + 1
-                results[label_index,10] += 1
-            if (get_label(letters, label_f2_adv_f)[0] == letter):
-                succ_f2_adv_f = succ_f2_adv_f + 1
-                results[label_index,11] += 1
-            if (get_label(letters, label_f2_adv_f2)[0] == letter):
-                succ_f2_adv_f2 = succ_f2_adv_f2 + 1
-                results[label_index,12] += 1
-
             total += 1
             results[label_index,0] += 1
 
@@ -167,15 +83,9 @@ def createResultsForModels(trained_model, trained_model_features, trained_model_
     results[36,1] = succ/total
     results[36,2] = succ_adv/total
     results[36,3] = succ_adv_f/total
-    results[36,4] = succ_adv_f2/total
     results[36,5] = succ_f/total
     results[36,6] = succ_f_adv/total
     results[36,7] = succ_f_adv_f/total
-    results[36,8] = succ_f_adv_f2/total
-    results[36,9] = succ_f2/total
-    results[36,10] = succ_f2_adv/total
-    results[36,11] = succ_f2_adv_f/total
-    results[36,12] = succ_f2_adv_f2/total
 
     print("FINISHED")
 
--- models.py
+++ models.py
@@ -214,19 +214,7 @@ def mLeNetDefault(input_shape, num_classes, steps_per_epoch, epochs, use_cache=F
         evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs)
         return model
 
-    model = Sequential()
-    # First convolution Layer
-    model.add(Conv2D(6, (5, 5), input_shape=input_shape)
-
-    model.add(AveragePooling2D(pool_size=(2,2)))
-
-    model.add(Conv2D(16, (5, 5)))
-
-    model.add(AveragePooling2D(pool_size=(2, 2)))  # Second Convolution Layer
-
-    model.add(Flatten())
-    model.add(Dense(120))
-    model.add(Dense(84))
+    model = getDefaultModel(input_shape)
 
     model.add(Dense(num_classes))
     model.add(Activation('softmax'))
@@ -243,6 +231,75 @@ def mLeNetDefault(input_shape, num_classes, steps_per_epoch, epochs, use_cache=F
 
     return model
 
+def mLeNetDefault_Features(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
+    model_name = 'LeNetDefault_Features'
+    helpers.createFoldersForModel(model_name, dataset)
+    print("===================== " + model_name + " model ====================")
+    if existsModelCache(model_name, dataset) and use_cache:
+        model = loadModel(model_name, dataset)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='rmsprop', metrics=['accuracy'])
+        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs)
+        return model
+
+    model = getDefaultModel(input_shape)
+    flat1 = Flatten()(model.layers[-1].output)
+
+    dense1 = Dense(units=256, activation="relu")(flat1)
+    classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(dense1)
+
+    featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
+
+    # define new model
+    model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
+    # summarize
+    model.summary()
+
+    n = 13 # number of features
+    k = num_classes #number of classes
+    cce = tf.keras.losses.CategoricalCrossentropy()
+    def featuresLossFunction(y_true, y_pred):
+        return K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:])
+
+    def classifierLossFunction(y_true, y_pred):
+        return cce(y_true[:,0:k], y_pred)
+
+    def classifierAccuracy(y_true,y_pred):
+        return tf.keras.metrics.sparse_categorical_accuracy(tf.math.argmax(y_true[:,0:k],1), y_pred)
+
+    def featuresAccuracy(y_true, y_pred):
+        
+        return K.sum(K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:]))
+
+    tf.executing_eagerly()
+
+    losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
+    metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
+    weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
+
+    model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
+    model.run_eagerly = True
+    [history, time_taken] = fitModel(
+        model, dataset, input_shape, steps_per_epoch, epochs, True)
+
+
+    if use_cache:
+        saveModel(model, model_name, dataset)
+
+    createAndSaveCurvesFeatures(history, model_name, dataset)
+
+    return model
+
+def getDefaultModel(input_shape):
+    model = Sequential()
+    model.add(Conv2D(6, (5, 5), input_shape=input_shape))
+    model.add(AveragePooling2D(pool_size=(2,2)))
+    model.add(Conv2D(16, (5, 5)))
+    model.add(AveragePooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    return model
 
 def fitModel(model, dataset, input_shape, batch_size, epochs, addCharacteristics):
     [training_filenames, GT_training] = load_image_names_and_labels(dataset, 'train', addCharacteristics)
