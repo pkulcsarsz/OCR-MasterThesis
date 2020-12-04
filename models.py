434fb703b78c1d2314747876e9cab3dfa10b0ac0
Peter Kulcsar Szabo <kulcsar@profit365.eu>
2020-12-04 11:47:42 +0100

    Models reafactored

--- models.py
+++ models.py
@@ -13,64 +13,28 @@ from data import load_image_names_and_labels
 import numpy as np
 
 
-def customLeNet(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
-    model_name = 'customLeNet'
+def mLeNetEnhanced_Features_Dense(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
+    model_name = 'LeNetEnhanced_Features_Dense'
     helpers.createFoldersForModel(model_name, dataset)
     print("===================== " + model_name + " model ====================")
-    if existsModelCache(model_name, dataset) and use_cache:
-        model = loadModel(model_name, dataset)
-        model.compile(loss='categorical_crossentropy',
-                      optimizer='rmsprop', metrics=['accuracy'])
-        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs)
-        return model
-
-    model = Sequential()
-    # First convolution Layer
-    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))  # Second Convolution Layer
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    flat1 = Flatten()(model.layers[-1].output)
-
-    dense1 = Dense(units=256, activation="relu")(flat1)
-    classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(dense1)
-
-    featuresOutput = Flatten(name='featuresOutput')(dense1)
-
-    # define new model
-    model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
-    # summarize
-    model.summary()
-
-    n = 13 # number of features
-    k = num_classes #number of classes
-    cce = tf.keras.losses.CategoricalCrossentropy()
-    def featuresLossFunction(y_true, y_pred):
-        return K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:])
-
-    def classifierLossFunction(y_true, y_pred):
-        return cce(y_true[:,0:k], y_pred)
-
-    def classifierAccuracy(y_true,y_pred):
-        return tf.keras.metrics.sparse_categorical_accuracy(tf.math.argmax(y_true[:,0:k],1), y_pred)
-
-    def featuresAccuracy(y_true, y_pred):
-        
-        return K.sum(K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:]))
-
-    tf.executing_eagerly()
-
     losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
     metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
     weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
 
+    if existsModelCache(model_name, dataset) and use_cache:
+        model = loadModel(model_name, dataset)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer='rmsprop', metrics=['accuracy'])
+        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs, True)
+        return model
+    
+    model = getDefaultEnhancedModel(input_shape)
+    dense1 = Dense(units=64, activation="relu")(model.layers[-1].output)
+    classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(dense1)
+    featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
+    # define new model
+    model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
+    
     model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
     model.run_eagerly = True
     [history, time_taken] = fitModel(
@@ -85,63 +49,27 @@ def customLeNet(input_shape, num_classes, steps_per_epoch, epochs, use_cache=Fal
     return model
 
 
-def customLeNet2(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
-    model_name = 'customLeNet2'
+def mLeNetEnhanced_Features(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
+    model_name = 'LeNetEnhanced_Features'
     helpers.createFoldersForModel(model_name, dataset)
     print("===================== " + model_name + " model ====================")
+    losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
+    metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
+    weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
+
     if existsModelCache(model_name, dataset) and use_cache:
         model = loadModel(model_name, dataset)
         model.compile(loss='categorical_crossentropy',
                       optimizer='rmsprop', metrics=['accuracy'])
-        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs)
+        evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs, True)
         return model
-
-    model = Sequential()
-    # First convolution Layer
-    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))  # Second Convolution Layer
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    flat1 = Flatten()(model.layers[-1].output)
-
-    dense1 = Dense(units=256, activation="relu")(flat1)
-    classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(dense1)
-
+    
+    model = getDefaultEnhancedModel(input_shape)
+    
+    classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(model.layers[-1].output)
     featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
-
     # define new model
     model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
-    # summarize
-    model.summary()
-
-    n = 13 # number of features
-    k = num_classes #number of classes
-    cce = tf.keras.losses.CategoricalCrossentropy()
-    def featuresLossFunction(y_true, y_pred):
-        return K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:])
-
-    def classifierLossFunction(y_true, y_pred):
-        return cce(y_true[:,0:k], y_pred)
-
-    def classifierAccuracy(y_true,y_pred):
-        return tf.keras.metrics.sparse_categorical_accuracy(tf.math.argmax(y_true[:,0:k],1), y_pred)
-
-    def featuresAccuracy(y_true, y_pred):
-        
-        return K.sum(K.square(y_pred[:,:n] - tf.cast(y_true, tf.float32)[:,k:]))
-
-    tf.executing_eagerly()
-
-    losses = {'classifierOutput': classifierLossFunction, 'featuresOutput': featuresLossFunction}
-    metrics = {'classifierOutput': classifierAccuracy, 'featuresOutput': featuresAccuracy}
-    weights = {'classifierOutput':1.0, 'featuresOutput':1.0}
 
     model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
     model.run_eagerly = True
@@ -158,7 +86,7 @@ def customLeNet2(input_shape, num_classes, steps_per_epoch, epochs, use_cache=Fa
 
 
 def mLeNetEnhanced(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
-    model_name = 'LeNet'
+    model_name = 'LeNetEnhanced'
     helpers.createFoldersForModel(model_name, dataset)
     print("===================== " + model_name + " model ====================")
     if existsModelCache(model_name, dataset) and use_cache:
@@ -167,25 +95,10 @@ def mLeNetEnhanced(input_shape, num_classes, steps_per_epoch, epochs, use_cache=
                       optimizer='rmsprop', metrics=['accuracy'])
         evaluateModel(model, dataset, input_shape, steps_per_epoch, epochs)
         return model
-
-    model = Sequential()
-    # First convolution Layer
-    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))  # Second Convolution Layer
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
+    
+    model = getDefaultEnhancedModel(input_shape)
     model.add(Flatten())
-    model.add(Dense(256))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    # model.add(Dense(256, activation="relu"))
 
     model.add(Dense(num_classes))
     model.add(Activation('softmax'))
@@ -247,14 +160,10 @@ def mLeNetDefault_Features(input_shape, num_classes, steps_per_epoch, epochs, us
         return model
 
     model = getDefaultModel(input_shape)
-    # flat1 = Flatten()(model.layers[-1].output)
-    # dense1 = Dense(units=256, activation="relu")(flat1)
     classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(model.layers[-1].output)
     featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
     # define new model
     model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
-    # summarize
-    model.summary()
 
     model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
     model.run_eagerly = True
@@ -269,7 +178,7 @@ def mLeNetDefault_Features(input_shape, num_classes, steps_per_epoch, epochs, us
     return model
     
 def mLeNetDefault_Features_Dense(input_shape, num_classes, steps_per_epoch, epochs, use_cache=False, dataset='dataset1'):
-    model_name = 'LeNetDefault_Features'
+    model_name = 'LeNetDefault_Features_Dense'
     helpers.createFoldersForModel(model_name, dataset)
     print("===================== " + model_name + " model ====================")
     
@@ -284,14 +193,11 @@ def mLeNetDefault_Features_Dense(input_shape, num_classes, steps_per_epoch, epoc
         return model
 
     model = getDefaultModel(input_shape)
-    # flat1 = Flatten()(model.layers[-1].output)
     dense1 = Dense(units=64, activation="relu")(model.layers[-1].output)
     classifierOutput = Dense(num_classes, activation='softmax', name='classifierOutput')(dense1)
     featuresOutput = Flatten(name='featuresOutput')(model.layers[-1].output)
     # define new model
     model = Model(inputs=model.inputs, outputs=[classifierOutput, featuresOutput])
-    # summarize
-    model.summary()
 
     model.compile(loss=losses,  metrics=metrics, optimizer='rmsprop', run_eagerly=True, loss_weights=weights)
     model.run_eagerly = True
@@ -314,6 +220,25 @@ def getDefaultModel(input_shape):
     model.add(Flatten())
     model.add(Dense(120))
     model.add(Dense(84))
+    return model
+
+def getDefaultEnhancedModel(input_shape):
+    model = Sequential()
+    # First convolution Layer
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))  # Second Convolution Layer
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+
     return model
 
 def fitModel(model, dataset, input_shape, batch_size, epochs, addCharacteristics):
@@ -344,9 +269,8 @@ def evaluateModel(model, dataset, input_shape, batch_size, epochs, addCharacteri
     scores = model.evaluate(my_validation_batch_generator)
     print(scores)
     if addCharacteristics:
-        print("%s: %.2f" % (model.metrics_names[2], scores[2]))
         print("%s: %.2f%%" % (model.metrics_names[3], scores[3]*100))
-        print("%s: %.2f%%" % (model.metrics_names[4], scores[4]))
+        print("%s: %.2f" % (model.metrics_names[4], scores[4]))
     else:
         print("%s: %.2f" % (model.metrics_names[0], scores[0]))
         print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
